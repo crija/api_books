28c39a7eaf416cbbb032eaa9ba1d71186de446fc
crija <crijacarla@gmail.com>
2024-04-26 20:31:10 -0300

    Remove unnecessary functions and add books list

--- routes/books.py
+++ routes/books.py
@@ -2,42 +2,45 @@ from flask import Blueprint, render_template
 
 books_route = Blueprint('books', __name__)
 
-# Lista os Livros
+books = [
+    {
+        "id": 1,
+        "title": "A máquina do caos",
+        "author": "Max Fisher"
+    },
+    {
+        "id": 2,
+        "title": "A estrada do futuro",
+        "author": "Bill Gates"
+    },
+    {
+        "id": 3,
+        "title": "Terra Americana",
+        "author": "Jeanine Cummins"
+    }
+]
+
+# List books
 @books_route.route('/')
 def book_list():
-   return{'página': "Lista_Livros"}
+   return books
 
-# Cadastra Livro
+# Create book
 @books_route.route('/', methods=['POST'] )
 def add_book():
-   pass
+   return '', 201
 
-# Renderiza Formulário
-@books_route.route('/new')
-def register_form():
-   return{'formulário': "Formulario_cadastro"}
-
-# Busca Livro
-@books_route.route('/')
+# Id book
+@books_route.route('/<int:book_id>')
 def get_book(book_id):
    pass
-    
-# Livros Id
-@books_route.route('/<int:book_id>')
-def book_id(book_id):
-   pass 
 
-# Formulário para editar cadastro
-@books_route.route('/<int:book_id>/edit')
-def form_edit(book_id):
-   pass 
-
-# Atualiza livro
+# Update book
 @books_route.route('/<int:book_id>/update', methods=['PUT'])
 def update_book(book_id):
-   pass 
+   pass
 
-# Deleta Livro
+# Delete book
 @books_route.route('/<int:book_id>/delete', methods=['DELETE'])
 def delete_book(book_id):
-   pass 
+   pass
