8e8333577c19649743cc0825410ae5462e377bcc
John Doe <crijacarla@gmail.com>
2024-04-29 20:00:09 -0300

    Remove route

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from routes.home import home_route
 from routes.books import books_route
 
 app = Flask(__name__)
 
-app.register_blueprint(home_route)
 app.register_blueprint(books_route, url_prefix='/books')
 
 # Execuxao
