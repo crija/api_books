ebcba3a8d637e21c74058e0138ed84f76acd886b
John Doe <crijacarla@gmail.com>
2024-05-23 21:28:52 -0300

    First functionality created "Function receives the get parameter to search for all books in the data case"

--- app_api_books/views.py
+++ app_api_books/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+import json
+
+from .models import Books
+from .serializer import BooksSerializer
+
 
 # Create your views here.
 
+@api_view(['GET'])
 def book_list(request):
-   return  render(request,'books.html')
+   if request.method == 'GET':
+        books = Books.objects.all()
+
+        serializer = BooksSerializer(books, many=True)
+        return Response(serializer.data)
+
+  #  return HttpResponse(status=status.HTTP_404_NOT_FOUND)
