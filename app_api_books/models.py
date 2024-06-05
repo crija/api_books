c635ada85f8429a9be6df68bf75252d93ae6467e
John Doe <crijacarla@gmail.com>
2024-06-05 09:24:23 -0300

    Add database table

--- app_api_books/models.py
+++ app_api_books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Books(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    number_of_pages = models.IntegerField(max_length=10, unique=False)
+    year_of_publication = models.IntegerField(max_length=6, unique=False)
+
