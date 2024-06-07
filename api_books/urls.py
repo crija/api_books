2cec3197990647b4d28727c736d604c8d9469deb
John Doe <crijacarla@gmail.com>
2024-06-07 10:58:06 -0300

    Include route to create books

--- api_books/urls.py
+++ api_books/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from app_api_books import views
+from django.urls import path, include
+from django.contrib import admin
+
+import app_api_books
+from app_api_books import urls
 
 urlpatterns = [
-    path('',views.book_list, name='book_list'),
+    path('admin/', admin.site.urls),
+    path('books/', include(app_api_books.urls), name= 'books'),
 ]
+
