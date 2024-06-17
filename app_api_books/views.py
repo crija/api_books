a5294a288a132661407ac6801595f8c168a01c34
John Doe <crijacarla@gmail.com>
2024-06-17 20:25:45 -0300

    Create filter to search book by title

--- app_api_books/views.py
+++ app_api_books/views.py
@@ -1,3 +1,7 @@
+# Buscar livros por título
+
+
+
 from django.http import HttpResponse, JsonResponse
 
 from rest_framework.decorators import api_view
@@ -18,7 +22,7 @@ def list_and_criate_books(request):
    if request.method == 'POST':
       new_book = request.data
       serializer = BooksSerializer(data=new_book)
-      if serializer.is_valid(raise_exception=True):
+      if serializer.is_valid():
          serializer.save()
 
          return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -49,6 +53,19 @@ def update_book(request, id):
       return Response(serializer.data)
 
    return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+def book_title(request):
+   if request.method == 'GET':                      
+      if request.GET['title']:                            # Verifica se existe o parametro 'title' na url
+         title_book = request.GET['title']                # Se o parametro existir, ele será guardado dentro da váriavel  'title_book'
+         title = Books.objects.filter(title=title_book)   # Pegar o objeto no banco de dados e guardar dentro da variavel 'title'
+         serializer = BooksSerializer(title, many=True)   # Transformar esse valor em json
+         return Response(serializer.data)                 # Retornar os dados serializados
+
+      else:
+         return Response(status=status.HTTP_400_BAD_REQUEST) # Se a url vier com parametro errado, retorna o status code 400
 
 
 
