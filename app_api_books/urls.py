4e44663b74f13937846a6c3e755c722ac4403cfe
John Doe <crijacarla@gmail.com>
2024-06-18 13:17:14 -0300

    Rewrite function names

--- app_api_books/urls.py
+++ app_api_books/urls.py
@@ -4,9 +4,8 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.list_and_criate_books, name= 'books'),
-    path('/<int:id>', views.update_book, name= 'update_book'),
-    path('/title', views.book_title)
+    path('', views.get_or_create_books, name= 'books'),
+    path('/<int:id>', views.get_or_delete_or_put_book, name= 'update_book'),
 
 ]
 
